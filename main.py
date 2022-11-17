0ab8c35b9a30fb911f5846fc00eb4aceedac08d7
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-11-17 23:25:36 +0000

    añadido el login

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
+import os
+import pymongo
+import time
 
+url = os.environ['url']
+cli = pymongo.MongoClient(url)
+db = cli["texpeed"]
+col = db["users"]
+while (True):
+    os.system("clear")
+    print("Bienvenido/a a Texpeed\nSelecciona una opcion\n\n1: Login\n2: Crear cuenta")
+    sel = input()
+    os.system("clear")
+    login = input("Login: ")
+    passw = input("Contraseña: ")
+    if (sel == "1" or sel == "login"):
+        try:
+            for x in col.find():
+                if x[login]:
+                    if x[login] != passw:
+                        raise IndexError
+                    else:
+                        break
+                else:
+                    raise IndexError
+        except Exception:
+            print("No se ha encontrado tu login o la contraseña esta mal")
+            continue
+        break
+    elif (sel == "2" or sel == "crear cuenta"):
+        for x in col.find():
+            if x[login]:
+                print("Ya hay un usuario con este login\n")
+                time.sleep(1)
+                break
+        col.insert_one({login: passw})
