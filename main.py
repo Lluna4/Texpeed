46e45f88bea2d746c382a095e665c5a151ee500e
Luna <69774917+carlyjb17@users.noreply.github.com>
2022-12-25 22:57:42 +0000

    arreglado el login

--- main.py
+++ main.py
@@ -2,35 +2,47 @@ import os
 import pymongo
 import time
 
-url = os.environ['url']
+url = os.environ['URL']
 cli = pymongo.MongoClient(url)
 db = cli["texpeed"]
-col = db["users"]
 while (True):
     os.system("clear")
     print("Bienvenido/a a Texpeed\nSelecciona una opcion\n\n1: Login\n2: Crear cuenta")
     sel = input()
     os.system("clear")
-    login = input("Login: ")
+    login = input("Usuario: ")
     passw = input("Contraseña: ")
     if (sel == "1" or sel == "login"):
-        try:
-            for x in col.find():
-                if x[login]:
-                    if x[login] != passw:
-                        raise IndexError
-                    else:
+        users = db.list_collection_names()
+        pas = 0
+        if login in users:
+            col = db[login]
+            for i in col.find():
+                try:
+                    if i["password"] == passw:
+                        pas = 1
                         break
-                else:
-                    raise IndexError
-        except Exception:
-            print("No se ha encontrado tu login o la contraseña esta mal")
-            continue
-        break
-    elif (sel == "2" or sel == "crear cuenta"):
-        for x in col.find():
-            if x[login]:
-                print("Ya hay un usuario con este login\n")
-                time.sleep(1)
+                except IndexError:
+                    pass
+            if pas == 1:
                 break
-        col.insert_one({login: passw})
+            else:
+                print("La contraseña no es correcta")
+                time.sleep(1)
+                continue    
+        else:
+            print("No se ha encontrado tu usuario")
+            time.sleep(1)
+            continue
+    elif (sel == "2" or sel == "crear cuenta"):
+        users = db.list_collection_names()
+        if login in users:
+            print("Este usuario ya tiene una cuenta")
+            time.sleep(1)
+            continue
+        else:
+            col = db[login]
+            col.insert_one({"password": passw})
+
+print("Lo conseguiste")
+exit()
